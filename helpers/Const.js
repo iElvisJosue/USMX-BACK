8c8e35e255c4fbbdee1f2aa338112d847bac122e
Elvis Josue <elvisjosue98@hotmail.com>
2024-10-23 15:58:47 -0600

    Se cambiaron los mensajes de respuesta

--- helpers/Const.js
+++ helpers/Const.js
@@ -1,10 +1,10 @@
-export const MENSAJE_DE_ERROR = `Lo sentimos, se ha producido un error interno en el servidor. Por favor inténtalo de nuevo ó inténtalo de nuevo más tarde.`;
+export const MENSAJE_DE_ERROR = `SERVIDOR: Se ha producido un error interno en el servidor. Por favor inténtalo de nuevo ó vuelve a intentarlo más tarde. ⌛`;
 
 export const MENSAJE_ERROR_CONSULTA_SQL =
-  "Ha ocurrido un error al realizar la consulta, por favor inténtalo de nuevo.";
+  "CONSULTA SQL: Ha ocurrido un error al realizar la consulta, por favor inténtalo de nuevo. 🔃";
 
 export const MENSAJE_DE_NO_AUTORIZADO =
-  "No estás autorizado para realizar esta operación. Si crees que esto es un error, contacta con el equipo de soporte.";
+  "AUTORIZACIÓN: No cuentas con los permisos necesarios para completar tu petición, por favor inicia sesión. 🔒";
 
 export const HOST = "https://www.usmxxpress.net";
 export const LINK_QR = "https://www.usmxxpress.net/NumeroDeGuia/";
