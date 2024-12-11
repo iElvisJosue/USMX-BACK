90f8ed578e4ef57e167091a4ca8a2a17e2699638
Elvis Josué <elvisjosue98@hotmail.com>
2024-12-10 18:27:23 -0600

    Ahora se permite el dominio de correos

--- initial/app.js
+++ initial/app.js
@@ -41,9 +41,9 @@ const __dirname = dirname(__filename);
 const app = express();
 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://usmx.vercel.app",
+  "https://usmxxpress.com",
   "https://www.usmxxpress.net",
+  "http://localhost:5173",
   "http://127.0.0.1:5500",
 ];
 
